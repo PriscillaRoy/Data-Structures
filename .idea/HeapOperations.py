11b386f17448f4a98c2615b0bc3259d809e989c1
Priscilla Roy Imandi <priscilla.roy94@gmail.com>
2018-10-09 23:04:41 -0500

    Day 5

---
+++ .idea/HeapOperations.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Heap:
+    def __init__(self):
+        self.nodelist =
+
+def insert_heap()
+
+
+
+
+arr = []
